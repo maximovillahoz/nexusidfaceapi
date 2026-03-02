9281488533a31f5e979d4ab9941d0f5ff7369475
maximovillahoz <maximovillahoz@gmail.com>
2026-03-02 13:38:32 +0100

    Arreglar dynamic ssr false en page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
-import NexusIDClient from "./components/NexusIDClient";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const NexusID = dynamic(() => import("./components/NexusID"), {
+  ssr: false,
+  loading: () => (
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#000000",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "'Courier New', monospace",
+        color: "#00ff41",
+        fontSize: 12,
+        letterSpacing: 3,
+      }}
+    >
+      INICIANDO NEXUS ID...
+    </div>
+  ),
+});
 
 export default function Page() {
-  return (
-    <main>
-      <NexusIDClient />
-    </main>
-  );
+  return <NexusID />;
 }
